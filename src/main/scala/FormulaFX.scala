ed62ee9b7b98981dabea4369a5f6480025da37e8
OndrejSpanel <OndrejSpanel@users.noreply.github.com>
2016-03-26 22:50:02 +0100

    Binding result to input.

--- src/main/scala/FormulaFX.scala
+++ src/main/scala/FormulaFX.scala
@@ -3,18 +3,23 @@ import scalafx.scene.Scene
 import scalafx.scene.control.TextField
 import scalafx.scene.layout.BorderPane
 
+//noinspection ForwardReference
 object FormulaFX extends JFXApp {
   stage = new JFXApp.PrimaryStage {
     title.value = "Formula Fx - Expression Calculator"
 
     scene = new Scene {
       val pane = new BorderPane {
-        val text = new TextField {
+        val input = new TextField {
           editable = true
+
         }
-        val result = new TextField
-        center = text
+        val result = new TextField {
+          text <== input.text
+        }
+        center = input
         bottom = result
+
       }
 
       root = pane
