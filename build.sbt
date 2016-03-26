880b457699ceac09f2905d14e5a8ea713f3d7e79
OndrejSpanel <OndrejSpanel@users.noreply.github.com>
2016-03-26 23:25:38 +0100

    Very basic expression parser implemented.

--- build.sbt
+++ build.sbt
@@ -6,6 +6,8 @@ scalaVersion := "2.11.8"
 
 libraryDependencies += "org.scalafx" %% "scalafx" % "8.0.60-R9"
 
+libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.4"
+
 libraryDependencies +=   "org.scalatest" %% "scalatest" % "2.2.4" % "test"
 
 // Fork a new JVM for 'run' and 'test:run', to avoid JavaFX double initialization problems
--- src/main/scala/com/github/opengrabeso/formulafx/Evaluate.scala
+++ src/main/scala/com/github/opengrabeso/formulafx/Evaluate.scala
@@ -1,8 +1,34 @@
 package com.github.opengrabeso.formulafx
 
+import scala.util.parsing.combinator.JavaTokenParsers
+
 object Evaluate {
+
+  object ExprParser extends JavaTokenParsers {
+    //def variable  : Parser[Double] = """[a-z]""".r ^^ { x => X(x) }
+    def number    : Parser[Double] = floatingPointNumber ^^ { x => x.toDouble }
+    def factor    : Parser[Double] = number | "(" ~> expr <~ ")"
+    def term      : Parser[Double] = factor ~ rep( "*" ~ factor | "/" ~ factor) ^^ {
+      case number ~ list => list.foldLeft(number) {
+        case (x, "*" ~ y) => x * y
+        case (x, "/" ~ y) => x / y
+      }
+    }
+    def expr  : Parser[Double] = term ~ rep("+" ~ term | "-" ~ term) ^^ {
+      case number ~ list => list.foldLeft(number) {
+        case (x, "+" ~ y) => x + y
+        case (x, "-" ~ y) => x - y
+      }
+    }
+
+    def apply(input: String): Double = parseAll(expr, input) match {
+      case Success(result, _) => result
+      case failure : NoSuccess => scala.sys.error(failure.msg)
+    }
+  }
+
   def apply(input: String): String = {
-    ":= " + input
+    ExprParser(input).toString
   }
 
 }
