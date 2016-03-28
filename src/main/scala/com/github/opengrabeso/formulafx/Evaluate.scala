462c269ddbf86f566bfaee5f6de857e1c029f018
OndrejSpanel <OndrejSpanel@users.noreply.github.com>
2016-03-28 21:43:03 +0200

    Working minutes and seconds parser.

--- src/main/scala/com/github/opengrabeso/formulafx/Evaluate.scala
+++ src/main/scala/com/github/opengrabeso/formulafx/Evaluate.scala
@@ -8,15 +8,23 @@ object Evaluate {
   var variables = Map[String, Number]()
 
   object ExprParser extends JavaTokenParsers {
+    def minutes: Parser[Number] = (wholeNumber <~ ":") ~ floatingPointNumber ^^ {case deg ~ min => deg.toInt + min.toDouble * (1.0/60)}
+    def minutesAndSeconds: Parser[Number] = (wholeNumber <~ ":") ~ (wholeNumber <~ ":") ~ floatingPointNumber ^^ {
+      case (deg ~ min ~ sec) => deg.toInt + min.toInt * (1.0/60) * sec.toDouble * (1.0/3600)
+    }
+
     def variable  : Parser[Number] = ident ^^ { x => variables.getOrElse(x, 0.0) }
-    def number    : Parser[Number] = floatingPointNumber ^^ { x => x.toDouble }
+    def fNumber   : Parser[Number] = floatingPointNumber ^^ { x => x.toDouble }
+    def number    : Parser[Number] = minutesAndSeconds | minutes | fNumber
     def factor    : Parser[Number] = (number | variable) | "(" ~> expr <~ ")"
+
     def term      : Parser[Number] = factor ~ rep( "*" ~ factor | "/" ~ factor) ^^ {
       case number ~ list => list.foldLeft(number) {
         case (x, "*" ~ y) => x * y
         case (x, "/" ~ y) => x / y
       }
     }
+
     def expr  : Parser[Number] = term ~ rep("+" ~ term | "-" ~ term) ^^ {
       case number ~ list => list.foldLeft(number) {
         case (x, "+" ~ y) => x + y
