84d8e1606b69566f7b2ff409b58d5ea7b761a361
OndrejSpanel <OndrejSpanel@users.noreply.github.com>
2016-03-28 21:58:07 +0200

    Extract operator definitions.

--- src/main/scala/com/github/opengrabeso/formulafx/Evaluate.scala
+++ src/main/scala/com/github/opengrabeso/formulafx/Evaluate.scala
@@ -8,6 +8,9 @@ object Evaluate {
   var variables = Map[String, Number]()
 
   object ExprParser extends JavaTokenParsers {
+
+    type Operator = (Number, Number) => Number
+
     def minutes: Parser[Number] = (wholeNumber <~ ":") ~ floatingPointNumber ^^ { case deg ~ min => deg.toInt + min.toDouble * (1.0 / 60) }
     def minutesAndSeconds: Parser[Number] = (wholeNumber <~ ":") ~ (wholeNumber <~ ":") ~ floatingPointNumber ^^ {
       case (deg ~ min ~ sec) => deg.toInt + min.toInt * (1.0 / 60) * sec.toDouble * (1.0 / 3600)
@@ -18,21 +21,20 @@ object Evaluate {
     def number: Parser[Number] = minutesAndSeconds | minutes | fNumber
     def factor: Parser[Number] = (number | variable) | "(" ~> expr <~ ")"
 
-    def mulOperators: Parser[String] = "*" | "/"
-    def addOperators: Parser[String] = "+" | "-"
+    def op_* : Parser[Operator] = "*" ^^^ { (a, b) => a * b }
+    def op_/ : Parser[Operator] = "/" ^^^ { (a, b) => a / b }
+    def op_+ : Parser[Operator] = "+" ^^^ { (a, b) => a + b }
+    def op_- : Parser[Operator] = "-" ^^^ { (a, b) => a - b }
+
+    def mulOperators: Parser[Operator] = op_* | op_/
+    def addOperators: Parser[Operator] = op_+ | op_-
 
     def term: Parser[Number] = factor ~ rep(mulOperators ~ factor) ^^ {
-      case number ~ list => list.foldLeft(number) {
-        case (x, "*" ~ y) => x * y
-        case (x, "/" ~ y) => x / y
-      }
+      case number ~ list => list.foldLeft(number) { case (x, op ~ y) => op(x, y) }
     }
 
     def expr: Parser[Number] = term ~ rep(addOperators ~ term) ^^ {
-      case number ~ list => list.foldLeft(number) {
-        case (x, "+" ~ y) => x + y
-        case (x, "-" ~ y) => x - y
-      }
+      case number ~ list => list.foldLeft(number) { case (x, op ~ y) => op(x, y) }
     }
 
     def assign: Parser[Number] = (ident <~ "=") ~ expr ^^ {
