ae6620fc4a624de62b75628d06db4916d81e54e5
OndrejSpanel <OndrejSpanel@users.noreply.github.com>
2016-03-28 21:43:47 +0200

    Reformat parser.

--- src/main/scala/com/github/opengrabeso/formulafx/Evaluate.scala
+++ src/main/scala/com/github/opengrabeso/formulafx/Evaluate.scala
@@ -8,24 +8,24 @@ object Evaluate {
   var variables = Map[String, Number]()
 
   object ExprParser extends JavaTokenParsers {
-    def minutes: Parser[Number] = (wholeNumber <~ ":") ~ floatingPointNumber ^^ {case deg ~ min => deg.toInt + min.toDouble * (1.0/60)}
+    def minutes: Parser[Number] = (wholeNumber <~ ":") ~ floatingPointNumber ^^ { case deg ~ min => deg.toInt + min.toDouble * (1.0 / 60) }
     def minutesAndSeconds: Parser[Number] = (wholeNumber <~ ":") ~ (wholeNumber <~ ":") ~ floatingPointNumber ^^ {
-      case (deg ~ min ~ sec) => deg.toInt + min.toInt * (1.0/60) * sec.toDouble * (1.0/3600)
+      case (deg ~ min ~ sec) => deg.toInt + min.toInt * (1.0 / 60) * sec.toDouble * (1.0 / 3600)
     }
 
-    def variable  : Parser[Number] = ident ^^ { x => variables.getOrElse(x, 0.0) }
-    def fNumber   : Parser[Number] = floatingPointNumber ^^ { x => x.toDouble }
-    def number    : Parser[Number] = minutesAndSeconds | minutes | fNumber
-    def factor    : Parser[Number] = (number | variable) | "(" ~> expr <~ ")"
+    def variable: Parser[Number] = ident ^^ { x => variables.getOrElse(x, 0.0) }
+    def fNumber: Parser[Number] = floatingPointNumber ^^ { x => x.toDouble }
+    def number: Parser[Number] = minutesAndSeconds | minutes | fNumber
+    def factor: Parser[Number] = (number | variable) | "(" ~> expr <~ ")"
 
-    def term      : Parser[Number] = factor ~ rep( "*" ~ factor | "/" ~ factor) ^^ {
+    def term: Parser[Number] = factor ~ rep("*" ~ factor | "/" ~ factor) ^^ {
       case number ~ list => list.foldLeft(number) {
         case (x, "*" ~ y) => x * y
         case (x, "/" ~ y) => x / y
       }
     }
 
-    def expr  : Parser[Number] = term ~ rep("+" ~ term | "-" ~ term) ^^ {
+    def expr: Parser[Number] = term ~ rep("+" ~ term | "-" ~ term) ^^ {
       case number ~ list => list.foldLeft(number) {
         case (x, "+" ~ y) => x + y
         case (x, "-" ~ y) => x - y
@@ -38,11 +38,11 @@ object Evaluate {
         x
     }
 
-    def command = assign | expr ^^ {x => x}
+    def command = assign | expr ^^ { x => x }
 
     def apply(input: String): Double = parseAll(command, input) match {
       case Success(result, _) => result
-      case failure : NoSuccess => 0
+      case failure: NoSuccess => 0
     }
   }
 
