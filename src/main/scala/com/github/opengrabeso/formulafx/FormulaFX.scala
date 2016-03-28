1e7a566a6871dbe2be07500d90ee142530282f04
OndrejSpanel <OndrejSpanel@users.noreply.github.com>
2016-03-28 21:29:04 +0200

    Trying to use table layout (not working, compile error).

--- src/main/scala/com/github/opengrabeso/formulafx/FormulaFX.scala
+++ src/main/scala/com/github/opengrabeso/formulafx/FormulaFX.scala
@@ -1,31 +1,49 @@
 package com.github.opengrabeso.formulafx
 
-import javafx.application.Platform
+import scalafx.application.Platform
 
+import scalafx.collections.ObservableBuffer
 import scalafx.application.JFXApp
+import scalafx.beans.property.StringProperty
 import scalafx.scene.Scene
-import scalafx.scene.control.TextField
+import scalafx.scene.control.{TableColumn, TableView, TextField}
 import scalafx.scene.layout.BorderPane
 
 object FormulaFX extends JFXApp {
   stage = new JFXApp.PrimaryStage {
     title.value = "Formula Fx - Expression Calculator"
 
+    case class TableRow(t: String) {
+      val text = new StringProperty(this, "text", t)
+    }
+
+    val tableData = ObservableBuffer[TableRow]()
+
     scene = new Scene {
       val pane = new BorderPane {
-        val result = new TextField {
+        val results = new TableView[TableRow](tableData) {
           editable = false
+
+          columns ++= List(
+            new TableColumn[TableRow, String] {
+              text = "Expression/Result"
+              cellValueFactory = _.value.text
+            }
+          )
         }
+
         val input = new TextField {
           editable = true
-          Platform.runLater(new Runnable {def run() = requestFocus()})
+          Platform.runLater(requestFocus())
 
           text.onChange {
-            result.text = Evaluate(text.value)
+            val result = Evaluate(text.value)
+            tableData.add(new TableRow(result))
+            ()
           }
 
         }
-        center = result
+        center = results
         bottom = input
 
       }
