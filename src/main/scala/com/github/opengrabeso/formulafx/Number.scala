61ef36f7aaf997fe57f6e82ed4a463a60ac1fe26
OndrejSpanel <OndrejSpanel@users.noreply.github.com>
2016-03-29 14:34:52 +0200

    Implemented minutes output formatting.

--- src/main/scala/com/github/opengrabeso/formulafx/Number.scala
+++ src/main/scala/com/github/opengrabeso/formulafx/Number.scala
@@ -11,10 +11,35 @@ object Format {
 
 import Format._
 
+object Number {
+  def toMinutesPos(x: Double) = {
+    assert(x >= 0)
+    val mins = x.toInt
+    val secs = (x - mins) * 60
+    val secsWhole = secs.toInt
+    // TODO: secs fracs
+    //val secsFrac = secs - secsWhole
+    //f"$mins:$secsWhole%02d$secsFrac%f"
+    val secsFrac = secs - secsWhole
+    f"$mins:$secsWhole%02d"
+  }
+
+  def toMinutes(x: Double): String = {
+    if (x < 0) "-" + toMinutesPos(-x)
+    else toMinutesPos(+x)
+  }
+
+  def toSeconds(x: Double) = {
+    toMinutes(x)
+  }
+
+}
 case class Number(x: Double, f: Format) {
+  import Number._
+
   override def toString = f match {
-    case Minutes => "M:" + x.toString
-    case Seconds => "S:" + x.toString
+    case Minutes => toMinutes(x)
+    case Seconds => toSeconds(x)
     case _ => x.toString
   }
 }
