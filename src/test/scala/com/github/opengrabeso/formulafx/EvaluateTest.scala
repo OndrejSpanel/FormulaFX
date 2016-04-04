f8a8328785d96f85943d585c8bf706d78348b660
OndrejSpanel <OndrejSpanel@users.noreply.github.com>
2016-04-04 14:02:04 +0200

    Added more equation tests.

--- src/test/scala/com/github/opengrabeso/formulafx/EvaluateTest.scala
+++ src/test/scala/com/github/opengrabeso/formulafx/EvaluateTest.scala
@@ -2,6 +2,8 @@ package com.github.opengrabeso.formulafx
 
 import org.scalatest.{FlatSpec, Matchers}
 
+import scala.util.Failure
+
 class EvaluateTest extends FlatSpec with Matchers with ExpressionTestUtils {
   behavior of "Evaluator with variables"
 
@@ -13,13 +15,27 @@ class EvaluateTest extends FlatSpec with Matchers with ExpressionTestUtils {
 
   it should "define a variable" in {
     val eval = createEvaluator
-    eval.apply("a=123") shouldBe res(123)
+    eval("a=123") shouldBe res(123)
     eval.settings.variables("a").x shouldBe 123.0
   }
 
   it should "define a variable on the right side" in {
     val eval = createEvaluator
-    eval.apply("12=a") shouldBe res(12)
+    eval("12=a") shouldBe res(12)
     eval.settings.variables("a").x shouldBe 12.0
+    eval("a=z") shouldBe res(12)
+    eval.settings.variables("z").x shouldBe 12.0
+  }
+
+  it should "redefine leftmost variable" in {
+    val eval = createEvaluator
+    eval("12=a") shouldBe res(12)
+    eval("a=456") shouldBe res(456)
+    eval.settings.variables("a").x shouldBe 456.0
+  }
+
+  it should "reject equation with multiple variables" in {
+    val eval = createEvaluator
+    eval("a=b") shouldBe a[Failure[_]]
   }
 }
