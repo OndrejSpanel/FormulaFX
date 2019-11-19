dfc88242f070942043f61e160b487405a419151f
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2019-11-19 02:11:57 +0100

    Initial swing version (no table yet)

--- build.sbt
+++ build.sbt
@@ -2,13 +2,6 @@ import sbtcrossproject.CrossPlugin.autoImport.{crossProject, CrossType}
 
 scalaVersion in ThisBuild := "2.11.8"
 
-// settings here used by IDE for highlighting
-libraryDependencies += "org.scalafx" %% "scalafx" % "8.0.60-R9"
-
-libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.4"
-
-libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0" % "test"
-
 def generateIndexTask(index: String, suffix: String) = Def.task {
   val source = baseDirectory.value / "index.html"
   val target = (crossTarget in Compile).value / index
@@ -22,6 +15,12 @@ def generateIndexTask(index: String, suffix: String) = Def.task {
   log.info(s"Generate $index with suffix: $suffix")
 }
 
+lazy val commonSettings = Seq(
+  scalaVersion := "2.11.8",
+  version := "0.1.1-alpha",
+  libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2",
+  libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.8" % "test"
+)
 lazy val root = project.in(file(".")).
   aggregate(pJVM, pJS).
   settings(
@@ -32,23 +31,17 @@ lazy val root = project.in(file(".")).
 lazy val projs = crossProject(JSPlatform, JVMPlatform).crossType(CrossType.Full)
   .in(file("."))
   .settings(
+    commonSettings,
     name := "FormulaFX",
-    version := "0.0.8-alpha",
     maxErrors := 1,
     scalacOptions := Seq("-unchecked", "-deprecation")
   )
   .jvmSettings(
     // Add JVM-specific settings here
-    libraryDependencies += "org.scalafx" %% "scalafx" % "8.0.60-R9",
-    libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.4",
-    libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0" % "test",
-    // Fork a new JVM for 'run' and 'test:run', to avoid JavaFX double initialization problems
-    fork := true
+    libraryDependencies += "org.scala-lang.modules" %% "scala-swing" % "2.1.1"
   )
   .jsSettings(
-    libraryDependencies += "org.scala-js" %%% "scala-parser-combinators" % "1.0.2",
     libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.0",
-    libraryDependencies += "org.scalatest" %%% "scalatest" % "3.0.0" % "test",
     (fastOptJS in Compile) := (fastOptJS in Compile).dependsOn(generateIndexTask("index-fast.html","fastOpt")).value,
     (fullOptJS in Compile) := (fullOptJS in Compile).dependsOn(generateIndexTask("index.html","opt")).value
   )
--- jvm/src/main/scala/com/github/opengrabeso/formulafx/FormulaFX.scala
+++ jvm/src/main/scala/com/github/opengrabeso/formulafx/FormulaFX.scala
@@ -1,40 +1,40 @@
 package com.github.opengrabeso.formulafx
 
-import core._
+import java.awt.event.{InputEvent, KeyEvent}
 
+import core._
 import java.util.prefs.Preferences
 
-import scalafx.application.Platform
-import scalafx.collections.ObservableBuffer
-import scalafx.application.JFXApp
-import scalafx.beans.property.StringProperty
-import scalafx.scene.Scene
-import scalafx.scene.control._
-import scalafx.scene.control.TableColumn._
-import scalafx.scene.control.MenuItem._
-import scalafx.Includes.{function12jfxCallback => _, _}
-import scalafx.scene.input._
-import scalafx.scene.layout.{BorderPane, VBox}
-import com.github.opengrabeso.scalafx.TextFieldAcceleratorFix
+import javax.swing.text.TableView
+import javax.swing.{SwingUtilities, UIManager}
 
-import scalafx.scene.image.Image
+import scala.collection.mutable
+import scala.swing.event.{EditDone, ValueChanged}
+import scala.swing.{BorderPanel, CheckMenuItem, Dimension, Label, MainFrame, Menu, MenuBar, MenuItem, SimpleSwingApplication, TextField, TextPane}
 
-case class TableRowText(t: String) {
-  val text = new StringProperty(this, "text", t)
-}
-
+case class TableRowText(text: String)
 //noinspection ForwardReference
-object FormulaFX extends JFXApp {
+object FormulaFX extends SimpleSwingApplication {
   def prefs: Preferences = Preferences.userRoot().node(getClass.getPackage.getName.toLowerCase)
 
-  stage = new JFXApp.PrimaryStage {
-    title.value = "Formula Fx - Expression Calculator"
+  override def startup(args: scala.Array[scala.Predef.String]): Unit = {
 
+    assert(SwingUtilities.isEventDispatchThread)
 
-    private val icon = new Image("/calculator.png")
-    icons.add(icon)
+    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName)
 
-    val tableData = ObservableBuffer[TableRowText]()
+    super.startup(args)
+  }
+
+  def top: MainFrame = new MainFrame {
+    title = "Formula Fx - Expression Calculator"
+
+    minimumSize = new Dimension(800, 600)
+
+    //private val icon = new Image("/calculator.png")
+    //icons.add(icon)
+
+    val tableData = mutable.ArrayBuffer.empty[TableRowText]
 
     def clearTable(): Unit = tableData.clear()
 
@@ -46,7 +46,7 @@ object FormulaFX extends JFXApp {
       prefs.putInt("rows", tableData.size)
       tableData.zipWithIndex.foreach {
         case (row,i) =>
-          prefs.put(rowId(i), row.text.value)
+          prefs.put(rowId(i), row.text)
       }
       for (i <- tableData.size until oldSize) {
         prefs.remove(rowId(i))
@@ -61,7 +61,7 @@ object FormulaFX extends JFXApp {
         tableData.clear()
         for (i <- 0 until rows) {
           val row = prefs.get(rowId(i), "")
-          tableData.add(TableRowText(row))
+          tableData += TableRowText(row)
           // we need to execute even lines so that variables are initialized
           if ((i%2) == 0) Evaluate.compute(row, false)
         }
@@ -70,127 +70,124 @@ object FormulaFX extends JFXApp {
 
     loadSession()
 
-    scene = new Scene {
-      def computeResult(preview: Boolean): Unit = {
-        val resultText = Evaluate.compute(input.text.value, preview)
+
+    val result = new TextField {
+      editable = false
+    }
+    val input = new TextField {
+      editable = true
+    }
+
+    def computeResult(preview: Boolean): Unit = {
+      val resultText = Evaluate.compute(input.text, preview)
+      resultText.map { res =>
+        result.text = res
+      }
+    }
+
+    listenTo(input)
+    reactions += {
+      case EditDone(`input`) =>
+        val resultText = Evaluate.compute(input.text, false)
         resultText.map { res =>
-          result.text = res
+          tableData += TableRowText(input.text)
+          tableData += TableRowText("  " + res)
+          input.text = ""
+          result.text = ""
+          saveSession()
         }
-      }
-
-      val result = new TextFieldAcceleratorFix {
-        editable = false
-      }
-      val input = new TextFieldAcceleratorFix {
-        editable = true
-        Platform.runLater(requestFocus())
-
-        onAction = handle {
-          val resultText = Evaluate.compute(text.value, false)
-          resultText.map { res =>
-            tableData.add(TableRowText(text.value))
-            tableData.add(TableRowText("  " + res))
-            text = ""
-            result.text = ""
-            saveSession()
-          }
-        }
-
-        text.onChange { computeResult(true)}
-
-      }
-      val statusBar = new Label
-
-      def changeSettings(change: => Unit): Unit = {
-        change
-        showStatus()
+      case ValueChanged(`input`) =>
         computeResult(true)
+    }
+    val statusBar = new Label
 
-      }
-      private val menuRadian = new CheckMenuItem("Radian") {
-        accelerator = new KeyCodeCombination(KeyCode.F9)
-        onAction = handle {changeSettings{Evaluate.angleUnitRadian()}}
-      }
-      private val menuDegree = new CheckMenuItem("Degree") {
-        accelerator = new KeyCodeCombination(KeyCode.F10)
-        onAction = handle {changeSettings{Evaluate.angleUnitDegree()}}
-      }
-
-      val menuBar = new MenuBar {
-        useSystemMenuBar = true
-        menus = Seq(
-          new Menu("File") {
-            items = Seq(
-              new MenuItem("Clear history") {
-                accelerator = new KeyCharacterCombination("N", KeyCombination.ControlDown)
-                onAction = handle {clearTable();Evaluate.clear()}
-              }
-            )
-          },
-          new Menu("Settings") {
-            items = Seq(
-              new Menu("Angle unit") {
-                items = Seq(menuRadian, menuDegree)
-              }
-            )
-          }
-        )
-      }
-
-      def showStatus(): Unit = {
-        statusBar.text.value = Evaluate.angleUnitName
-        menuDegree.selected = Evaluate.angleUnitIsDegree
-        menuRadian.selected = Evaluate.angleUnitIsRadian
-      }
-
-      val pane = new BorderPane {
-
-        val results = new TableView[TableRowText](tableData) { table =>
-          editable = false
-
-          placeholder = new Label("")
-
-          columnResizePolicy = TableView.ConstrainedResizePolicy
-
-          def useSelectedRow(): Unit = {
-            val row = table.selectionModel.value.getSelectedItem
-            input.text = row.text.value
-          }
-
-          columns += new TableColumn[TableRowText, String] {
-            maxWidth = Int.MaxValue // http://stackoverflow.com/posts/35265368/edit
-            text = "Expression/Result"
-            sortable = false
-            cellValueFactory = {_.getValue.text}
-          }
-
-          rowFactory = t => new TableRow[TableRowText] {
-            onMouseClicked = (me: MouseEvent) => {
-              if (me.button == MouseButton.PRIMARY && me.clickCount == 2) {
-                useSelectedRow()
-              }
-            }
-          }
-
-          contextMenu = new ContextMenu {
-            items += new MenuItem {
-              text = "Use"
-              onAction = handle {
-                useSelectedRow()
-              }
-            }
-          }
-        }
-
-        top = menuBar
-        center = results
-        bottom = new VBox(input, result, statusBar)
-
-      }
-
-      root = pane
+    private val menuRadian = new CheckMenuItem("Radian") {
+      //accelerator = new KeyCodeCombination(KeyCode.F9)
+      //onAction = handle {changeSettings{Evaluate.angleUnitRadian()}}
+    }
+    private val menuDegree = new CheckMenuItem("Degree") {
+      //accelerator = new KeyCodeCombination(KeyCode.F10)
+      //onAction = handle {changeSettings{Evaluate.angleUnitDegree()}}
+    }
+    def changeSettings(change: => Unit): Unit = {
+      change
       showStatus()
+      computeResult(true)
 
     }
+    val menu = new MenuBar {
+      contents += new Menu("Settings") {
+        contents += new Menu("Angle unit") {
+          contents += menuRadian
+          contents += menuDegree
+        }
+      }
+
+      contents +=  new Menu("File") {
+        contents += new MenuItem(new ActionWithCode("Clear history", {clearTable(); Evaluate.clear()}, KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK))
+      }
+    }
+
+    menuBar = menu
+
+    def showStatus(): Unit = {
+      statusBar.text = Evaluate.angleUnitName
+      menuDegree.selected = Evaluate.angleUnitIsDegree
+      menuRadian.selected = Evaluate.angleUnitIsRadian
+    }
+
+    val pane = new BorderPanel {
+      import BorderPanel.Position._
+
+      /*
+      val results = new TableView[TableRowText](tableData) { table =>
+        editable = false
+
+        placeholder = new Label("")
+
+        columnResizePolicy = TableView.ConstrainedResizePolicy
+
+        def useSelectedRow(): Unit = {
+          val row = table.selectionModel.value.getSelectedItem
+          input.text = row.text.value
+        }
+
+        columns += new TableColumn[TableRowText, String] {
+          maxWidth = Int.MaxValue // http://stackoverflow.com/posts/35265368/edit
+          text = "Expression/Result"
+          sortable = false
+          cellValueFactory = {_.getValue.text}
+        }
+
+        rowFactory = t => new TableRow[TableRowText] {
+          onMouseClicked = (me: MouseEvent) => {
+            if (me.button == MouseButton.PRIMARY && me.clickCount == 2) {
+              useSelectedRow()
+            }
+          }
+        }
+
+        contextMenu = new ContextMenu {
+          items += new MenuItem {
+            text = "Use"
+            onAction = handle {
+              useSelectedRow()
+            }
+          }
+        }
+      }
+
+      add(results, Center)
+      center = results
+
+       */
+      add(input, Center)
+      add(result, South)
+      // TODO: statusBar
+    }
+
+    contents = pane
+    showStatus()
+
   }
 }
